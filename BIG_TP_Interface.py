ab3f346a992ccd0d79bf762749241496a10cfa96
rcym <racym.ouldhammouda@gmail.com>
2022-12-25 18:29:50 +0100

    round brk

--- BIG_TP_Interface.py
+++ BIG_TP_Interface.py
@@ -737,15 +737,15 @@ def show_BIG_TP1():
                 Label(PxyContainer, text="Calcule des entropies Conditionnelles :", font=("Arial 18 bold underline"), bg="#333", fg="#fff", justify=LEFT).grid(row=1, column=0, padx=10, sticky=W)
                 Label(PxyContainer, text="Methode 1 :", font=("Arial 18 bold"), bg="#333", fg="#fff", justify=LEFT).grid(row=2, column=0, padx=10, sticky=W)
                 Label(PxyContainer, text="On utilise la formule : H(X/Y) = somme(P(xi) .somme(P(y/w).log(1/P(y/x))))", font=("Arial 16"), bg="#333", fg="#fff", justify=LEFT).grid(row=3, column=0, padx=10, sticky=W)
-                Label(PxyContainer, text="===> H(X/Y) = "+str(Hxsy) + "(bits/symboles)", font=("Arial 16"), bg="#333", fg="#fff", justify=LEFT).grid(row=4, column=0, padx=10, sticky=W)
-                Label(PxyContainer, text="===> H(Y/X) = "+str(Hysx) + "(bits/symboles)", font=("Arial 16"), bg="#333", fg="#fff", justify=LEFT).grid(row=5, column=0, padx=10, sticky=W)
+                Label(PxyContainer, text="===> H(X/Y) = "+str(Hxsy) + " (bits/symboles)", font=("Arial 16"), bg="#333", fg="#fff", justify=LEFT).grid(row=4, column=0, padx=10, sticky=W)
+                Label(PxyContainer, text="===> H(Y/X) = "+str(Hysx) + " (bits/symboles)", font=("Arial 16"), bg="#333", fg="#fff", justify=LEFT).grid(row=5, column=0, padx=10, sticky=W)
 
                 Label(PxyContainer, text=" ", bg="#333").grid(row=6, column=0, padx=10, sticky=W) # espace
                 
                 Label(PxyContainer, text="Methode 2 :", font=("Arial 18 bold"), bg="#333", fg="#fff", justify=LEFT).grid(row=7, column=0, padx=10, sticky=W)
                 Label(PxyContainer, text="On utilise la formule de l'entropie : H(Y/X) = H(X,Y) - H(X)", font=("Arial 16"), bg="#333", fg="#fff", justify=LEFT).grid(row=8, column=0, padx=10, sticky=W)
-                Label(PxyContainer, text="===> H(Y/X) = "+str(Hysx_H) + "(bits/symboles)", font=("Arial 16"), bg="#333", fg="#fff", justify=LEFT).grid(row=9, column=0, padx=10, sticky=W)
-                Label(PxyContainer, text="===> H(X/Y) = "+str(Hxsy_H) + "(bits/symboles)", font=("Arial 16"), bg="#333", fg="#fff", justify=LEFT).grid(row=10, column=0, padx=10, sticky=W)
+                Label(PxyContainer, text="===> H(Y/X) = "+str(round(Hysx_H)) + " (bits/symboles)", font=("Arial 16"), bg="#333", fg="#fff", justify=LEFT).grid(row=9, column=0, padx=10, sticky=W)
+                Label(PxyContainer, text="===> H(X/Y) = "+str(round(Hxsy_H)) + " (bits/symboles)", font=("Arial 16"), bg="#333", fg="#fff", justify=LEFT).grid(row=10, column=0, padx=10, sticky=W)
 
 
 
