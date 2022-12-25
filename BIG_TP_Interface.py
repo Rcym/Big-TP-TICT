ad819d22bfb53d038a839eae2bbe631fc7fa2053
rcym <racym.ouldhammouda@gmail.com>
2022-12-25 10:53:45 +0100

    menu Proba finished

--- BIG_TP_Interface.py
+++ BIG_TP_Interface.py
@@ -6,10 +6,69 @@ import math
 def Entropie(Proba):
     H = 0
     for i in range(0, len(Proba)):
-        H += -Proba[i]/100 * math.log2(Proba[i]/100)
+        H += -Proba[i] * math.log2(Proba[i])
     return round(H, 3)
-    
 
+def EntropieConjointe(Proba_xy):
+    Hxy = 0
+    for i in range(0, len(Proba_xy)):
+        for j in range(0, len(Proba_xy[i])):
+            Hxy += -(Proba_xy[i][j]) * math.log2(Proba_xy[i][j])
+    return round(Hxy, 3)
+
+# Fonction pour calculer la quantité d'information mutuelle
+def QuantitéInfoMutuelle(Proba1, Proba2):
+    Ixy = []
+    for i in range(0, len(Proba1)):
+        Ixy.append([])
+        for j in range(0, len(Proba2)):
+            Ixy[i].append(round(-math.log2((Proba1[i])*(Proba2[j])), 3))
+    return
+
+# Function pour calculer l'entropie conditionnelle
+def EntropieConditionnelle(PX, P_YSX, YXorXY):
+    if YXorXY == "YsX":
+        Hcond = 0
+        for i in range(0, len(PX)):
+            for j in range(0, len(P_YSX[i])):
+                Hcond += -(PX[i]) * (P_YSX[j][i]) * math.log2(P_YSX[j][i])
+        return round(Hcond, 3)
+    elif YXorXY == "XsY":
+        Hcond = 0
+        for i in range(0, len(PX)):
+            for j in range(0, len(P_YSX[i])):
+                Hcond += -(PX[i]) * (P_YSX[i][j]) * math.log2(P_YSX[i][j])
+        return round(Hcond, 3)
+
+def ProbaCond(Pxy, Px, N1, N2, direction):
+    if direction == "ysx":
+        Proba_YsX = []
+        for i in range(0, N1):
+            Pysx_In = []
+            for j in range(0, N2):
+                Inside = Pxy[i][j] / Px[i]
+                Inside = round(Inside, 3)
+                Pysx_In.append(Inside)
+            Proba_YsX.append(Pysx_In)
+        return Proba_YsX
+    elif direction == "xsy":
+        Proba_YsX = []
+        for i in range(0, N1):
+            Pysx_In = []
+            for j in range(0, N2):
+                Inside = Pxy[i][j] / Px[j]
+                Inside = round(Inside, 3)
+                Pysx_In.append(Inside)
+            Proba_YsX.append(Pysx_In)
+        return Proba_YsX
+
+def erreurPopup(currentWindow):
+    errWindow = Toplevel(currentWindow)
+    errWindow.configure(bg="red")
+    Label(errWindow, text="Une erreur lors de la saisie des donnes veuillez reesayer", font=("Arial", 14)).pack(padx=8,pady=8)
+    return
+
+    
 root = Tk()
 root.title("BIG TP Interface")
 root.geometry("500x500")
@@ -215,6 +274,7 @@ def show_BIG_TP1():
         entryWindow.geometry("800x600")
 
 
+
         # Showing scalable entry fields for Pxy
         wrapper1 = LabelFrame(entryWindow)
         wrapper1.configure(bg="#333")
@@ -222,36 +282,52 @@ def show_BIG_TP1():
 
         theCanvas = Canvas(wrapper1)
         theCanvas.configure(bg="#222")
-        theCanvas.pack()
+        #theCanvas.pack()
+        theCanvas.grid(row=0, column=0)
 
         yscrollbar = ttk.Scrollbar(wrapper1, orient=VERTICAL, command=theCanvas.yview)
-        yscrollbar.pack(side=RIGHT, fill=Y)
+        #yscrollbar.pack(side=RIGHT, fill=Y)
+        yscrollbar.grid(row=0,column=1, sticky=NS)
         xscrollbar = ttk.Scrollbar(wrapper1, orient=HORIZONTAL, command=theCanvas.xview)
-        xscrollbar.pack(side=BOTTOM, fill=X)#, expand="yes")
+        #xscrollbar.pack(side=BOTTOM, fill=X)#, expand="yes")
+        xscrollbar.grid(row=1, column=0, sticky=EW)
 
         theCanvas.configure(yscrollcommand=yscrollbar.set)
         theCanvas.bind("<Configure>", lambda e: theCanvas.configure(scrollregion=theCanvas.bbox("all")))
         theCanvas.bind_all("<MouseWheel>", lambda event: theCanvas.yview_scroll(int(-1*(event.delta/120)), "units"))
 
+        # Taille fenétre dynamique
+        #taille_x = 83 * tailleSource_1
+        #taille_y = 34 * tailleSource_2
+        #if taille_x > 600: taille_x = 600
+        #if taille_y > 400: taille_y = 400
+
+        #if tailleSource_1 <=4: taille_x = 200
+        #if tailleSource_2 <=4: taille_x = 200
+
+
         theFrame = Frame(theCanvas)
         theFrame.configure(bg="#222")
         theCanvas.create_window((0,0), window=theFrame, anchor="nw")
+        #theCanvas.configure(width=taille_x, height=taille_y, bg="#222")             # <-- size canvas
         theCanvas.configure(width=600, height=400, bg="#222")             # <-- size canvas
 
         # preparing table entry
-        for i in range(tailleSource_1):
-            th = Label(theFrame, text="P(x" + str(i+1) + ",yj)", font=("Arial",14), bg="#444", fg="#fff")
-            th.grid(row=0, column=i+1)
+        for i in range(tailleSource_2):
+            #th = Label(theFrame, text="P(xi,y" + str(i+1) + ")", font=("Arial",14), bg="#444", fg="#fff")
+            th = Label(theFrame, text="y" + str(i+1), font=("Arial",14), bg="#444", fg="#fff")
+            th.grid(row=0, column=i+1, sticky=NSEW)
         for j in range(tailleSource_1):
-            th = Label(theFrame, text="P(xi,y" + str(j+1) + ")", font=("Arial",14), bg="#444", fg="#fff")
-            th.grid(row=j+1, column=0)
+            #th = Label(theFrame, text="P(x" + str(j+1) + ",yj)", font=("Arial",14), bg="#444", fg="#fff")
+            th = Label(theFrame, text="x" + str(j+1), font=("Arial",14), bg="#444", fg="#fff")
+            th.grid(row=j+1, column=0, sticky=NSEW)
 
         for i in range(tailleSource_1):
             laColonne = []
             for j in range(tailleSource_2):
                 td = Entry(theFrame)
                 td.configure(font=("Arial", 12), width=5)
-                td.grid(row=i+1, column=j+1)
+                td.grid(row=i+1, column=j+1, padx=2)
                 laColonne.append(td)
             Pxy.append(laColonne)
 
@@ -267,9 +343,284 @@ def show_BIG_TP1():
             
 
     def validerPxy():
+        # Verification matrice entré
         global tailleSource_1
         global tailleSource_2
-        print(tailleSource_1)
+        
+        sommePxy = 0
+        for i in range(tailleSource_1):
+            for j in range(tailleSource_2):
+                try:
+                    sommePxy += float(Pxy[i][j].get())
+                except:
+                    erreurPopup()
+                    return
+        if sommePxy != 1:
+            errWindow = Toplevel(entryWindow)
+            errWindow.configure(bg="red")
+            Label(errWindow, text="La somme des probabilité doit étre égale a 1", font=("Arial", 14)).pack(padx=8,pady=8)
+            return
+        
+        # Transforming Pxy into values instead of using .get()
+        for i in range(tailleSource_1):
+            for j in range(tailleSource_2):
+                Pxy[i][j] = float(Pxy[i][j].get())
+        
+        # Calcul Px et Py
+        Px = []
+        Py = []
+        for i in range(tailleSource_1):
+            Px_temp = 0
+            for j in range(tailleSource_2):
+                Px_temp += Pxy[i][j]
+            Px.append(round(Px_temp,2))
+        for i in range(tailleSource_2):
+            Py_temp = 0
+            for j in range(tailleSource_1):
+                Py_temp += Pxy[j][i]
+            Py.append(round(Py_temp,2))
+
+        ## Calcule de TOUT
+        global Pxsy
+        global Pysx
+        global Hx
+        global Hy
+
+        Pysx = ProbaCond(Pxy, Px, tailleSource_1, tailleSource_2, "ysx")
+        Pxsy = ProbaCond(Pxy, Py, tailleSource_1, tailleSource_2, "xsy")
+        Hx = Entropie(Px)
+        Hy = Entropie(Py)
+
+        # Creating Display Window TP1
+        entryWindow.destroy()
+        displayWindow = Toplevel(root)
+        displayWindow.geometry("1000x600")
+        displayWindow.resizable(False,False)
+        displayWindow.configure(bg="#333")
+        
+        def switchToX():
+            X_btn.configure(bg="#fff", fg="#333")
+            Y_btn.configure(bg="#333", fg="#fff")
+            isXselected = True
+            displaySource("toX")
+        def switchToY():
+            X_btn.configure(bg="#333", fg="#fff")
+            Y_btn.configure(bg="#fff", fg="#333")
+            isXselected = False
+            displaySource("toY")
+
+        # Sources display section
+        sourceDisplay = LabelFrame(displayWindow, bg="#222", width=200, height=600)
+        sourceDisplay.pack(side=RIGHT, fill=Y)
+        Label(sourceDisplay, text="  Sources :  ", font=("Arial",20), bg="#222", fg="#fff").pack(pady=10)
+        btnFrame_source = Frame(sourceDisplay, bg="#222")
+        btnFrame_source.pack()
+        X_btn = Button(btnFrame_source,text="Source X", bg="#fff", fg="#333", bd=3, font="bold", command=switchToX)
+        Y_btn = Button(btnFrame_source,text="Source Y", bg="#333", fg="#fff", bd=3, font="bold", command=switchToY)
+        X_btn.grid(row=0, column=0, padx=4)
+        Y_btn.grid(row=0, column=1, padx=4)
+        Label(sourceDisplay, text=" ", font=("Arial",24), bg="#222").pack(pady=2)
+
+        def displaySource(toWhere):
+            if toWhere == "" or toWhere == "toX":
+                isXselected = True
+            else:
+                isXselected = False
+
+            tailleToUse = tailleSource_1 if isXselected else tailleSource_2
+            symbToUse = "X" if isXselected else "Y"
+            P = Px if isXselected else Py
+
+            global rapper
+            global Qinfo_frame
+            try:
+                rapper.destroy()
+                Qinfo_frame.destroy()
+            except:
+                pass
+
+            ################################### scroll bar section
+            rapper = LabelFrame(sourceDisplay, height=100)
+            rapper.configure(bg="#333")
+            rapper.pack(fill="y", expand="no")
+
+            theCanvas = Canvas(rapper)
+            theCanvas.configure(bg="#222")
+            theCanvas.pack(side=LEFT)
+
+            yscrollbar = ttk.Scrollbar(rapper, orient=VERTICAL, command=theCanvas.yview)
+            yscrollbar.pack(side=RIGHT, fill=Y)
+
+            theCanvas.configure(yscrollcommand=yscrollbar.set)
+            theCanvas.bind("<Configure>", lambda e: theCanvas.configure(scrollregion=theCanvas.bbox("all")))
+            theCanvas.bind_all("<MouseWheel>", lambda event: theCanvas.yview_scroll(int(-1*(event.delta/120)), "units"))
+
+            theFrame = Frame(theCanvas)
+            theFrame.configure(bg="#222")
+            theCanvas.create_window((0,0), window=theFrame, anchor="nw")
+            theCanvas.configure(width=210, bg="#222")
+            ################################### end of scroll bar section
+
+
+            for i in range(tailleToUse):
+                toWrite = "P(" + symbToUse + str(i) + ") = " + str(P[i])
+                addedLabel = Label(theFrame, text=toWrite, bg="#222", fg="#fff", font=("Arial",16)).pack(padx=20, pady=5)
+            
+            Qinfo_frame = Frame(sourceDisplay, bg="#222")
+            Label(Qinfo_frame, text="Entropie :      ", bg="#222", fg="#fff", font=("Arial",16)).pack()
+            labelAntropie_text = "H(" + symbToUse + ") = " + str(Entropie(P))
+            labelEntropie = Label(Qinfo_frame, text=labelAntropie_text, bg="#222", fg="#fff", font=("Arial",16))
+            labelEntropie.pack()
+            Qinfo_frame.pack(pady=10)
+
+        displaySource("")
+
+
+        # Genral display
+        generalDisplay = Frame(displayWindow, bg="#333")
+        generalDisplay.pack(side=RIGHT, fill=BOTH, expand=YES)
+        generalButtonFrame = Label(generalDisplay, bg="#444")
+        generalButtonFrame.pack(side=TOP, fill=X, pady=10)
+
+        # general buttons
+        Qinfo_btn = Button(generalButtonFrame, text="Quantité d'information", font=("Arial",18), bg="#fff", fg="#333", bd=3, command=lambda: generalChange("Qinfo"))
+        Proba_btn = Button(generalButtonFrame, text="Probabilitées", font=("Arial",18), bg="#333", fg="#fff", bd=3, command=lambda: generalChange("Proba"))
+        Entropie_btn = Button(generalButtonFrame, text="Entropie", font=("Arial",18), bg="#333", fg="#fff", bd=3, command=lambda: generalChange("Entropie"))
+
+        Qinfo_btn.grid(row=0, column=0, padx=5)
+        Proba_btn.grid(row=0, column=1, padx=5)
+        Entropie_btn.grid(row=0, column=2, padx=5)
+
+        global generalMainFrame
+        generalMainFrame = Frame(generalDisplay, bg="#333")
+        generalMainFrame.pack(side=BOTTOM, fill=BOTH, expand=YES)
+
+        def generalChange(btn):
+            try:
+                global generalMainFrame
+                generalMainFrame.destroy()
+                generalMainFrame = Frame(generalDisplay, bg="#333")
+                generalMainFrame.pack(side=BOTTOM, fill=BOTH, expand=YES)
+            except:
+                pass
+
+
+            if btn == "Qinfo":
+                Qinfo_btn.configure(bg="#fff", fg="#333")
+                Proba_btn.configure(bg="#333", fg="#fff")
+                Entropie_btn.configure(bg="#333", fg="#fff")
+            elif btn == "Proba":
+                Qinfo_btn.configure(bg="#333", fg="#fff")
+                Proba_btn.configure(bg="#fff", fg="#333")
+                Entropie_btn.configure(bg="#333", fg="#fff")
+
+                secondGeneralBtnFrame = Frame(generalMainFrame, bg="#444")
+                secondGeneralBtnFrame.pack(pady=15, fill=X)
+
+                # Proba buttons
+                Pxy_btn = Button(secondGeneralBtnFrame, text="P(x,y)", font=("Arial",16), bg="#fff", fg="#333", bd=3, command=lambda: probaChange("Pxy"))
+                Pxsy_btn = Button(secondGeneralBtnFrame, text="P(x/y)", font=("Arial",16), bg="#333", fg="#fff", bd=3, command=lambda: probaChange("Pxsy"))
+                Pysx_btn = Button(secondGeneralBtnFrame, text="P(y/x)", font=("Arial",16), bg="#333", fg="#fff", bd=3, command=lambda: probaChange("Pysx"))
+
+                Pxy_btn.grid(row=0, column=0, padx=5)
+                Pxsy_btn.grid(row=0, column=1, padx=5)
+                Pysx_btn.grid(row=0, column=2, padx=5)
+
+                
+
+                def probaChange(btn):
+                    global probaScrol
+                    try:
+                        probaScrol.destroy()
+                    except:
+                        pass
+                    ################################### scroll bar section
+                    probaScrol = LabelFrame(generalMainFrame, height=400)
+                    probaScrol.configure(bg="#333")
+                    probaScrol.pack(fill="y", expand="no", pady=20)
+
+                    theCanvas = Canvas(probaScrol)
+                    theCanvas.configure(bg="#222")
+                    theCanvas.pack(side=LEFT)
+
+                    yscrollbar = ttk.Scrollbar(probaScrol, orient=VERTICAL, command=theCanvas.yview)
+                    yscrollbar.pack(side=RIGHT, fill=Y)
+
+                    theCanvas.configure(yscrollcommand=yscrollbar.set)
+                    theCanvas.bind("<Configure>", lambda e: theCanvas.configure(scrollregion=theCanvas.bbox("all")))
+                    theCanvas.bind_all("<MouseWheel>", lambda event: theCanvas.yview_scroll(int(-1*(event.delta/120)), "units"))
+
+                    thePxyFrame = Frame(theCanvas)
+                    thePxyFrame.configure(bg="#222")
+                    theCanvas.create_window((0,0), window=thePxyFrame, anchor="nw")
+                    theCanvas.configure(width=600,height=400 , bg="#222")
+                    ################################### end of scroll bar section
+
+                    # preparting the table
+                    for i in range(tailleSource_2):
+                        th = Label(thePxyFrame, text="y" + str(i+1), font=("Arial",14), bg="#444", fg="#fff")
+                        th.grid(row=0, column=i+1, sticky=NSEW)
+                    for j in range(tailleSource_1):
+                        th = Label(thePxyFrame, text="x" + str(j+1), font=("Arial",14), bg="#444", fg="#fff")
+                        th.grid(row=j+1, column=0, sticky=NSEW)
+
+                    if btn == "Pxy":
+                        Pxy_btn.configure(bg="#fff", fg="#333")
+                        Pxsy_btn.configure(bg="#333", fg="#fff")
+                        Pysx_btn.configure(bg="#333", fg="#fff")
+
+                        # top left corder (title)
+                        th = Label(thePxyFrame, text="P(x,y)", font=("Arial 14 bold"), bg="#444", fg="#fff")
+                        th.grid(row=0, column=0, sticky=NSEW)
+
+                        for i in range(tailleSource_1):
+                            for j in range(tailleSource_2):
+                                toWrite = str(Pxy[i][j])
+                                Label(thePxyFrame, text=toWrite, font=("Arial",12,), bg="#222", fg="#fff").grid(row=i+1,column=j+1, padx=5, pady=6)
+
+                    elif btn == "Pxsy":
+                        Pxy_btn.configure(bg="#333", fg="#fff")
+                        Pxsy_btn.configure(bg="#fff", fg="#333")
+                        Pysx_btn.configure(bg="#333", fg="#fff")
+
+                        # top left corder (title)
+                        th = Label(thePxyFrame, text="P(x/y)", font=("Arial 14 bold"), bg="#444", fg="#fff")
+                        th.grid(row=0, column=0, sticky=NSEW)
+
+                        for i in range(tailleSource_1):
+                            for j in range(tailleSource_2):
+                                toWrite = str(Pxsy[i][j])
+                                Label(thePxyFrame, text=toWrite, font=("Arial",12,), bg="#222", fg="#fff").grid(row=i+1,column=j+1, padx=5, pady=6)
+
+                    elif btn == "Pysx":
+                        Pxy_btn.configure(bg="#333", fg="#fff")
+                        Pxsy_btn.configure(bg="#333", fg="#fff")
+                        Pysx_btn.configure(bg="#fff", fg="#333")
+
+                        # top left corder (title)
+                        th = Label(thePxyFrame, text="P(y/x)", font=("Arial 14 bold"), bg="#444", fg="#fff")
+                        th.grid(row=0, column=0, sticky=NSEW)
+
+
+                        for i in range(tailleSource_1):
+                            for j in range(tailleSource_2):
+                                toWrite = str(Pysx[i][j])
+                                Label(thePxyFrame, text=toWrite, font=("Arial",12,), bg="#222", fg="#fff").grid(row=i+1,column=j+1, padx=5, pady=6)
+
+                probaChange("Pxy")
+
+            elif btn == "Entropie":
+                Qinfo_btn.configure(bg="#333", fg="#fff")
+                Proba_btn.configure(bg="#333", fg="#fff")
+                Entropie_btn.configure(bg="#fff", fg="#333")
+
+
+
+
+            
+
+
+
 
 
 
@@ -319,6 +670,8 @@ def show_BIG_TP1():
     text2.grid(row=0, column=0)
     entrySource2.grid(row=0, column=1)
     entrySource2_frame.grid(row=1, column=0)
+
+
 
     
 
